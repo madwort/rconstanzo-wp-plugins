9b0627621312cf6ab2b2ca98735f1c32c50455c4
Tom Ward <tomward@fmail.co.uk>
2015-09-01 09:16:47 +0100

    JS for springy-links menu lives in own file

---
+++ springy_links_plugin/springy-links.js
@@ -0,0 +1,71 @@
+function drawSpringyMenu(parentName, graph, width, height) {
+  var force = d3.layout.force()
+      .size([width, height])
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .linkDistance(180)
+      .charge(-60);
+
+  var svg = d3.select(parentName).append('svg')
+      .attr('id','springy-menu')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('xlink','http://www.w3.org/1999/xlink');
+
+  var link = svg.selectAll('.link')
+    .data(graph.links)
+    .enter().append('line')
+    .attr('class', 'link');
+
+  var node = svg.selectAll('g.node')
+    .data(graph.nodes)
+    .enter().append('g')
+    .attr('class','node');
+
+  function linkify() {
+    return node.append('svg:a')
+      .attr('xlink:href', function(d){ return '.' + d.url; });
+  }
+
+  linkify().append('circle')
+    // radius of the circles
+    .attr('r', 10);
+
+  linkify().append('text')
+    .text(function(d){
+      return d.title;
+    })
+    .attr('class','title');
+
+  linkify().append('text')
+    .text(function(d){
+      return d.subtitle;
+    })
+    .attr('class','subtitle');
+
+  node.call(force.drag);
+
+  force.on('tick',function(){
+    link.attr('x1', function(d) { return d.source.x; })
+        .attr('y1', function(d) { return d.source.y; })
+        .attr('x2', function(d) { return d.target.x; })
+        .attr('y2', function(d) { return d.target.y; });
+
+    d3.selectAll('circle')
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; });
+
+    d3.selectAll('text')
+        .attr('x', function(d) { return (d.x-(this.getBBox().width/2)); });
+
+    d3.selectAll('text.title')
+        .attr('y', function(d) { return (d.y-40); });
+
+    d3.selectAll('text.subtitle')
+        .attr('y', function(d) { return (d.y-20); });
+
+  })
+
+  force.start();
+
+}
--- springy_links_plugin/springy-links.php
+++ springy_links_plugin/springy-links.php
@@ -8,12 +8,14 @@ Author: MADWORT
 Author URI: http://www.madwort.co.uk
 */
 
-function d3_scripts()
+function springy_links_scripts()
 {
 		wp_register_script( 'd3', plugins_url( '/d3.js', __FILE__ ) );
 		wp_enqueue_script( 'd3' );
+		wp_register_script( 'springy-links', plugins_url( '/springy-links.js', __FILE__ ) );
+		wp_enqueue_script( 'springy-links' );
 }
-add_action( 'wp_enqueue_scripts', 'd3_scripts' );
+add_action( 'wp_enqueue_scripts', 'springy_links_scripts' );
 
 function springy_menu_style()
 {
@@ -27,91 +29,15 @@ add_shortcode('springy-menu', 'springy_menu_handler');
 
 function springy_menu_handler($atts)
 {
-	return "
-	<script>
+	return "<script>
 
   var width = 650,
       height = 400;
 
   d3.json('" . plugins_url( 'menu.json', __FILE__ ) . "', function(error, graph) {
-    drawMenu(graph);
+    drawSpringyMenu('.entry-content', graph, width, height);
   });
-  
-  var node;
-
-  function drawMenu(graph) {
-    var force = d3.layout.force()
-        .size([width, height])
-        .nodes(graph.nodes)
-        .links(graph.links)
-        .linkDistance(180)
-        .charge(-60);
-
-    var svg = d3.select('.entry-content').append('svg')
-        .attr('id','springy-menu')
-        .attr('width', width)
-        .attr('height', height)
-        .attr('xlink','http://www.w3.org/1999/xlink');
-
-    var link = svg.selectAll('.link')
-      .data(graph.links)
-      .enter().append('line')
-      .attr('class', 'link');
-
-    node = svg.selectAll('g.node')
-      .data(graph.nodes)
-      .enter().append('g')
-      .attr('class','node');
-
-    function linkify() {
-      return node.append('svg:a')
-        .attr('xlink:href', function(d){ return '.' + d.url; });
-    }
-
-    linkify().append('circle')
-      // radius of the circles
-      .attr('r', 10);
-
-    linkify().append('text')
-      .text(function(d){
-        return d.title;
-      })
-      .attr('class','title');
-
-    linkify().append('text')
-      .text(function(d){
-        return d.subtitle;
-      })
-      .attr('class','subtitle');
-
-    node.call(force.drag);
-
-    force.on('tick',function(){
-      link.attr('x1', function(d) { return d.source.x; })
-          .attr('y1', function(d) { return d.source.y; })
-          .attr('x2', function(d) { return d.target.x; })
-          .attr('y2', function(d) { return d.target.y; });
-
-      d3.selectAll('circle')
-          .attr('cx', function(d) { return d.x; })
-          .attr('cy', function(d) { return d.y; });
-
-      d3.selectAll('text')
-          .attr('x', function(d) { return (d.x-(this.getBBox().width/2)); });
-
-      d3.selectAll('text.title')
-          .attr('y', function(d) { return (d.y-40); });
-
-      d3.selectAll('text.subtitle')
-          .attr('y', function(d) { return (d.y-20); });
-
-    })
-
-    force.start();
-
-  }
-  </script>";
+	</script>";
 }
-
 
 ?>
