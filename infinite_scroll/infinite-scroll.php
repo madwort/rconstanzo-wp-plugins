d5c50a4010f6eeedc0516e486ab3eca9e6cfeffb
Tom Ward <tomward@fmail.co.uk>
2015-09-03 17:07:16 +0100

    IS: remove infinite generator & replace with file
    * TODO: filename is hardcoded, needs to be param
    * might be a bit buggy, depending on context

--- infinite_scroll/infinite-scroll.php
+++ infinite_scroll/infinite-scroll.php
@@ -3,23 +3,23 @@
 Plugin Name: Infinite Scroll
 Plugin URI: http://www.rodrigoconstanzo.com/thesis/
 Description: Scroll..scroll..scroll..FOREVER!!!!
-Version: 0.2
+Version: 0.3
 Author: MADWORT
 Author URI: http://www.madwort.co.uk
 */
 
 function infinite_scroll_scripts()
 {
-		wp_register_script( 'infinite-scroll', plugins_url( '/infinite-scroll.js', __FILE__ ), array('jquery'), "01" );
-		wp_enqueue_script( 'infinite-scroll' );
+	wp_register_script( 'infinite-scroll', plugins_url( '/infinite-scroll.js', __FILE__ ), array('jquery'), "03" );
+	wp_enqueue_script( 'infinite-scroll' );
 }
 add_action( 'wp_enqueue_scripts', 'infinite_scroll_scripts' );
 
 function infinite_scroll_style()
 {
-    wp_register_style( 'infinite-scroll-style', plugins_url( '/infinite-scroll.css', __FILE__ ));
+  wp_register_style( 'infinite-scroll-style', plugins_url( '/infinite-scroll.css', __FILE__ ));
 
-    wp_enqueue_style( 'infinite-scroll-style' );
+  wp_enqueue_style( 'infinite-scroll-style' );
 }
 add_action( 'wp_enqueue_scripts', 'infinite_scroll_style' );
 
@@ -27,7 +27,7 @@ add_shortcode('infinite-scroll', 'infinite_scroll_handler');
 
 function infinite_scroll_handler($atts)
 {
-	return "<div id='scroll-to-read'><p id='continue-reading'> Scroll down to continue reading. </p></div>";
+ return "<div id='scroll-to-read'><p>The process</p></div>";
 }
 
 ?>
