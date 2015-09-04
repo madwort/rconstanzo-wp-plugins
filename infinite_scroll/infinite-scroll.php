0f7f41a6c5592dc50a4b2510f4ff502c3d5731ec
Tom Ward <tomward@fmail.co.uk>
2015-09-04 10:10:20 +0100

    IS: Version bump 0.4

--- infinite_scroll/infinite-scroll.php
+++ infinite_scroll/infinite-scroll.php
@@ -3,14 +3,14 @@
 Plugin Name: Infinite Scroll
 Plugin URI: http://www.rodrigoconstanzo.com/thesis/
 Description: Scroll..scroll..scroll..FOREVER!!!!
-Version: 0.3
+Version: 0.4
 Author: MADWORT
 Author URI: http://www.madwort.co.uk
 */
 
 function infinite_scroll_scripts()
 {
-	wp_register_script( 'infinite-scroll', plugins_url( '/infinite-scroll.js', __FILE__ ), array('jquery'), "03" );
+	wp_register_script( 'infinite-scroll', plugins_url( '/infinite-scroll.js', __FILE__ ), array('jquery'), "04" );
 	wp_enqueue_script( 'infinite-scroll' );
 }
 add_action( 'wp_enqueue_scripts', 'infinite_scroll_scripts' );
