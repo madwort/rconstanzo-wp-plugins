7bc2c274f58dc44497c7d5c8e7d015f81b29acbc
Tom Ward <tomward@fmail.co.uk>
2015-09-10 16:31:25 +0100

    Silly hover handlers!

--- tag_cloud/tag_cloud.js
+++ tag_cloud/tag_cloud.js
@@ -1,3 +1,19 @@
 ( function($) {
+  function create_tag_handlers() {
+    $("#tag_cloud .tag").css('line-height','2em').each(function (t) {
+      // console.log($(this));
+      $(this)
+        .mouseenter(function (e) {
+          console.log("in", e, this);
+          $(this).animate({"font-size": "2em", "line-height": "1em"},300)
+        })
+        .mouseleave(function (e) {
+          console.log("out", e, this);
+          $(this).animate({"font-size": "1em", "line-height": "2em"},300)
+        });
+      });
+  };
+
+  $(create_tag_handlers);
 
 } )( jQuery );
