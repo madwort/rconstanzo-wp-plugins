1b93153cd8c83fcfeee0c7516b53dea118565591
Tom Ward <tomward@fmail.co.uk>
2015-09-10 13:49:40 +0100

    Add new template for tag_cloud project

---
+++ tag_cloud/tag_cloud.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Plugin Name: Tag cloud
+Plugin URI: http://www.rodrigoconstanzo.com/thesis/
+Description: The amazing dynamic tag cloud
+Version: 0.1
+Author: MADWORT
+Author URI: http://www.madwort.co.uk
+*/
+
+function tag_cloud_scripts()
+{
+    wp_register_script( 'tag_cloud',
+   plugins_url( '/tag_cloud.js',
+   __FILE__ ),
+   array('jquery'),
+   "01" );
+    wp_enqueue_script( 'tag_cloud' );
+}
+add_action( 'wp_enqueue_scripts', 'tag_cloud_scripts' );
+
+// Don't need any styles right now, but might do later...
+// function tag_cloud_style()
+// {
+//     wp_register_style( 'tag_cloud-style', plugins_url( '/tag_cloud.css', __FILE__ ));
+//
+//     wp_enqueue_style( 'tag_cloud-style' );
+// }
+// add_action( 'wp_enqueue_scripts', 'tag_cloud_style' );
+
+add_shortcode('tag_cloud', 'tag_cloud_handler');
+
+function tag_cloud_handler($atts)
+{
+  $a = shortcode_atts( 
+          array(
+            'path' => plugins_url( '/tag_cloud_assets/', __FILE__ ),
+          ), $atts 
+        );
+
+  return "
+    <div id='tag_cloud'>
+      [
+        composition,
+        improvisation,
+        performance,
+        diy,
+        software,
+        controller,
+        mapping,
+        gesture,
+        video,
+        analysis,
+        framework,
+        feedback,
+        noise,
+        interaction,
+        behavior,
+        game,
+        battle,
+        dfscore,
+        networked score,
+        drums
+      ]    
+    </div>
+      ";
+}
+
+?>
