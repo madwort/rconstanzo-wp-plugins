ad230d9e0023cb706a536e361f75a6594f7901f1
Tom Ward <tomward@fmail.co.uk>
2015-09-19 20:12:09 +0100

    ID: make infinite destruction a bit more targetted

--- infinite_destruction/infinite-destruction.js
+++ infinite_destruction/infinite-destruction.js
@@ -10,22 +10,46 @@
     }, delay);
   }
 
+  function remove_random_element(elements) {
+    var random_element_id = Math.floor(elements.length*Math.random());
+    var random_element = $(elements[random_element_id]);
+    var max_font_size = 5;
+    if (random_element.is('a')) {
+      // implies li.menu-item > a
+      max_font_size = 2;
+    };
+    var font_size = Math.floor(1+(Math.random()*max_font_size));
+    random_element
+      .replaceWith("<p style='font-size: " + font_size + 
+                   "em; margin: 0;'>The process</p>");
+  }
+
   function handle_scroll() {
-    var all_elements = $('body').find('*');
-    var random_element_id = Math.floor(all_elements.length*Math.random());
-    var random_element = $(all_elements[random_element_id]);
-    if(
-      !(random_element.hasClass('wp-image-2616')) 
-      &&
-      !(random_element.hasClass('colLeft')) 
-      &&
-      !(random_element.hasClass('content')) 
-    ) {
-      var font_size = Math.floor(1+(Math.random()*10));
-      random_element
-        .replaceWith("<p style='font-size: " + font_size + 
-                     "em;'>The process</p>");
-    }
+    // Phase 1: text, navbar, widgets
+    // * div#colLeft > p
+    //  * apart from div.colLeft > p > img !!!
+    //  * done with :last-child
+    // * li.menu-item > a
+    // * div.rightBox > ul > li
+    // * div.rightBox > div.textwidget
+
+    var my_elements = $('div#colLeft > p').not(':last-child');
+
+    var new_elements = $('li.menu-item > a');
+    my_elements = $.merge(my_elements, new_elements)
+
+    new_elements = $('div.rightBox > ul > li');
+    my_elements = $.merge(my_elements, new_elements)
+
+    new_elements = $('div.rightBox > div.textwidget');
+    my_elements = $.merge(my_elements, new_elements)
+
+    // console.log(my_elements.length);
+    remove_random_element(my_elements);
+
+    // TODO: implement phase 2...
+    // Phase 2: divs apart from .colLeft, .colRight & .content
+
   }
 
   w.destruction = function () {
