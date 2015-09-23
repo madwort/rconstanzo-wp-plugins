05ef8b121864c02ac726a8352eb52c7001303210
Tom Ward <tomward@fmail.co.uk>
2015-09-23 14:20:10 +0100

    Quite nice functioning first layout

--- restructuring_piece_project_diagram/restructuring-piece-project-diagram.js
+++ restructuring_piece_project_diagram/restructuring-piece-project-diagram.js
@@ -1,11 +1,23 @@
 (function (w, d3) {
   'use strict';
-  w.drawPieceProjectDiagram = function (parentName, graph, 
-                                        connections, width, height) 
+  w.drawPieceProjectDiagram = function (parentName, nodes, 
+                                        links, width, height) 
   {
     var object_size = 40;
 
     var buttonDiv = d3.select(parentName).append('div');
+
+    var nodeMap = d3.map(nodes, function(d) { return d.id; });
+
+    links.forEach(function (d){
+      d.source = nodeMap.get(d.source);
+      d.target = nodeMap.get(d.target);
+    })
+
+    var CONCEPTUAL = 1;
+    var TEMPORAL = 2;
+    var TECHNICAL = 3;
+    var AESTHETIC = 4;
 
     buttonDiv.append('button')
       .text('Conceptual')
@@ -65,8 +77,13 @@
         ;
     }
 
+    var link_lines = svg.selectAll('.link')
+                        .data(links)
+                        .enter().append('line')
+                        .attr('class','link');
+
     var piece = svg.selectAll('g.piece')
-      .data(graph)
+      .data(nodes)
       .enter().append('g')
       .attr('class','piece')
       .attr('id', function(d) { return d.id; });
@@ -90,7 +107,11 @@
 
       force1 = d3.layout.force()
           .size([width, height])
-          .nodes(graph);
+          .nodes(nodes)
+          .links(links)
+          .gravity(0.1)
+          .charge(-150)
+          .linkDistance(50);
 
       force1.on('tick',function(){
         var myParent = d3.select('svg#restructuring-piece-project-diagram');
@@ -102,11 +123,19 @@
         myParent.selectAll('text')
             .attr('x', function(d) { return (d.x-(this.getBBox().width/2)); })
             .attr('y', function(d) { return d.y; });
+
+        myParent.selectAll('.link')
+            .attr('x1', function(d) { return d.source.x; })
+            .attr('y1', function(d) { return d.source.y; })
+            .attr('x2', function(d) { return d.target.x; })
+            .attr('y2', function(d) { return d.target.y; });
+
       })
 
     }
 
     create_draggable_layout();
+    start_draggable_layout();
 
     function start_draggable_layout() {
       piece.call(force1.drag);
@@ -120,37 +149,46 @@
 
     function transition_to_layout(target_layout) {
 
-      stop_draggable_layout();
-
-      function get_layout_for_state(d) {
-        return d.layouts[target_layout];
-      }
-
-      function get_state_x(d) {
-        return get_layout_for_state(d).x;
-      }
-
-      function get_state_y(d) {
-        return get_layout_for_state(d).y;
-      }
-
-      var myparent = d3.select('svg#restructuring-piece-project-diagram');
-
-      myparent.selectAll('g').transition()
-        .attr('x', get_state_x)
-        .attr('y', get_state_y);
-
-      myparent.selectAll('g.piece circle').transition()
-        .attr('cx', get_state_x)
-        .attr('cy', get_state_y);
-
-      myparent.selectAll('g text').transition()
-        .attr('x', function(d) { return (get_state_x(d)-(this.getBBox().width/2)); })
-        .attr('y', get_state_y);
+      console.log(links);
+      var my_links = links.filter(function (d) {
+        return parseInt(d.mode) == target_layout;
+      })
+      
+      console.log(my_links);
+      // link.data(my_links)
+      force1.links(my_links);
+      
+      // stop_draggable_layout();
+      //
+      // function get_layout_for_state(d) {
+      //   return d.layouts[target_layout];
+      // }
+      //
+      // function get_state_x(d) {
+      //   return get_layout_for_state(d).x;
+      // }
+      //
+      // function get_state_y(d) {
+      //   return get_layout_for_state(d).y;
+      // }
+      //
+      // var myparent = d3.select('svg#restructuring-piece-project-diagram');
+      //
+      // myparent.selectAll('g').transition()
+      //   .attr('x', get_state_x)
+      //   .attr('y', get_state_y);
+      //
+      // myparent.selectAll('g.piece circle').transition()
+      //   .attr('cx', get_state_x)
+      //   .attr('cy', get_state_y);
+      //
+      // myparent.selectAll('g text').transition()
+      //   .attr('x', function(d) { return (get_state_x(d)-(this.getBBox().width/2)); })
+      //   .attr('y', get_state_y);
 
     }
 
-    transition_to_layout('conceptual');
+    transition_to_layout(CONCEPTUAL);
 
   };
 } )( window, d3 );
