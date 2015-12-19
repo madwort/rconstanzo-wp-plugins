d68d907ddbd6ea8d0b136e45c42200e2ccecae47
Tom Ward <tomward@fmail.co.uk>
2015-12-19 15:53:13 +0000

    IS: Version bump 0.8

--- infinite_scroll/infinite-scroll.php
+++ infinite_scroll/infinite-scroll.php
@@ -3,7 +3,7 @@
 Plugin Name: Thesis Infinite Scroll
 Plugin URI: http://www.rodrigoconstanzo.com/thesis/
 Description: Scroll..scroll..scroll..FOREVER!!!!
-Version: 0.7
+Version: 0.8
 Author: MADWORT
 Author URI: http://www.madwort.co.uk
 */
