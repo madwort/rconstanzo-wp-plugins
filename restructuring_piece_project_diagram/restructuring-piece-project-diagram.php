10ee9bb59cd5873368d1d1d3eeeea38f26eed5e4
Tom Ward <tomward@fmail.co.uk>
2016-06-16 20:51:32 +0100

    RPPD: version bump 0.5

--- restructuring_piece_project_diagram/restructuring-piece-project-diagram.php
+++ restructuring_piece_project_diagram/restructuring-piece-project-diagram.php
@@ -3,7 +3,7 @@
 Plugin Name: Restructuring Piece/Project Diagram
 Plugin URI: http://www.rodrigoconstanzo.com/thesis/
 Description: whizzy diagram thing
-Version: 0.4
+Version: 0.5
 Author: MADWORT
 Author URI: http://www.madwort.co.uk
 */
