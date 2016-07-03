641155a32f52b4df69ad47eb8ad2c2831e37ff3b
Tom Ward <tomward@fmail.co.uk>
2016-07-04 00:17:26 +0100

    TC: rework with new syntax
    * functions correctly in Firefox 47, Safari untested

--- tag_cloud/tag_cloud_search.js
+++ tag_cloud/tag_cloud_search.js
@@ -3,11 +3,14 @@
     var params = window.location.hash.split(',');
     var search_term = params[0].replace('#','');
     var search_id = parseInt(params[1]) - 1;
+    var result = $('p').filter(':contains(' + search_term + ')')
+                  .filter(function(index) {return index === search_id; });
 
-    var results = $('p').filter(':contains(' + search_term + ')');
-    var target = $(results[search_id]).offset().top;
+    var target = $(result).offset().top;
+    console.log(target);
     // console.log('scroll to', search_term, search_id, target);
-    $('body').scrollTop(target);
+    // $('body').scrollTop(target);
+    $('body,html').stop(true,true).animate({scrollTop: target}, 10);
     // console.log('scrolled');
   };
 
